6f1bcccc7f93df3dbd786f487cec95f7baf17fdc
Carlos Gaviria <cgaviriamarquez@gmail.com>
2025-11-25 08:14:43 -0500

    Conexion con sonarcloude

--- lib/ui/login/loginpage.dart
+++ lib/ui/login/loginpage.dart
@@ -36,7 +36,7 @@ class _LoginPageState extends State<LoginPage> {
     } else if (rolSeleccionado == "administrador") {
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => const Homepage_Admin()),
+        MaterialPageRoute(builder: (context) => const HomepageAdmin()),
       );
     }
   } else {
